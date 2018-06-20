b19e21e6d695127f6fc4321b69dc4c1791b173d1
Farzad <farzadrahman59@gmail.com>
2018-06-21 00:55:51 +0600

    Farzad

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,5 +61,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'CORS' => \App\Http\Middleware\CORS::class,
+        'jwt.auth' => 'Tymon\JWTAuth\Middleware\GetUserFromToken',
+        'jwt.refresh' => 'Tymon\JWTAuth\Middleware\RefreshToken',
     ];
 }
